1159e5f4edb1ba07a9078211d4705a415e65598f
Pratyush Tiwary <54539159+piyushtiwary@users.noreply.github.com>
2023-01-03 12:16:48 +0530

    Update project.py

--- project.py
+++ project.py
@@ -6,14 +6,15 @@ count = 0
 li = list(b.split(" "))
 
 j  = 0
+
 while(j<len(b)):
 	if(b[j] == " "):  
 		count+=1
-	j+=1	
+	j += 1	
 
 realCount = (count+1)
 main = " "
 for v in range(realCount):
 	main = main+li[v][0]
 
-print("Acronym is:",main.upper())
+print("Acronym is: ",main.upper())
